ceb330d2455c4b1c2071ca9ec4b97edc788e13a6
ACuteWoof <sutharshanvithushan@gmail.com>
2022-12-15 23:52:35 +0530

    gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import Header from "../components/header";
 import Footer from "../components/footer";
@@ -34,4 +35,5 @@ function Gallery() {
     </>
   );
 }
+
 export default Gallery;
